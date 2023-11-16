1b9e851890a835a9fab2a4638085883a399f12cc
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-11-16 14:40:48 +0000

    Fixed Math.Pi instead of Math.PI.

--- public/src/update.js
+++ public/src/update.js
@@ -4,23 +4,19 @@
  *    mat4 matrixes.
  */
 function update(context, matrixInfo, stepTime) {
-  const {
-      modelMatrix,
-      viewMatrix,
-      projectionMatrix,
-      modelViewMatrix,
-    } = matrixInfo;
 
-  const FOV = 45 * Math.Pi / 180;
-  const RADIUS = context.canvas.innerWidth / context.canvas.innerHeight;
+  const FOV = 45 * Math.PI / 180;
+  const RADIUS = context.canvas.clientWidth / context.canvas.clientHeight;
   const NEAR = 0.1;
-  const FAR = undefined;
-  glMatrix.mat4.perspective(projectionMatrix, FOV, RADIUS, NEAR, FAR);
+  const FAR = null;
+  glMatrix.mat4.perspective(matrixInfo.projectionMatrix, FOV, RADIUS, NEAR, FAR);
 
   const VELOCITY = 50 / 1000;
-  const DELTA = (stepTime * VELOCITY) * Math.pi / 180;
+  const DELTA = (stepTime * VELOCITY) % 360;
   const axis = glMatrix.vec3.fromValues(0, 1.0, 0);
-  glMatrix.mat4.rotate(modelMatrix, modelMatrix, DELTA * Math.Pi / 180, axis);
+  glMatrix.mat4.rotate(matrixInfo.modelMatrix, matrixInfo.modelMatrix,
+      DELTA * Math.Pi / 180, axis);
 
-  glMatrix.mat4.multiply(modelViewMatrix, modelMatrix, viewMatrix);
+  glMatrix.mat4.multiply(matrixInfo.modelViewMatrix, matrixInfo.modelMatrix,
+      matrixInfo.viewMatrix);
 }
